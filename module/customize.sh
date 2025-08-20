673ed322defca99a83cffb643d6ed393b3afe3de
Tools-app <localhost.hutao@gmail.com>
2025-08-20 20:42:33 +0800

    refactor: Optimize customize scripts
    Signed-off-by: Tools-app <localhost.hutao@gmail.com>

--- module/customize.sh
+++ module/customize.sh
@@ -1,4 +1,9 @@
+SKIPUNZIP=1
+
 ui_print "安装中"
+sleep 0.1
+ui_print "解压文件中"
+unzip -o "$ZIPFILE" -x "META-INF/*" -d "$MODPATH"
 
 set_perm_recursive $MODPATH 0 0 0755 0644
-set_perm  $MODPATH/oukaro 0 0 0755
+set_perm $MODPATH/oukaro 0 0 0755
